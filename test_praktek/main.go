2a2206e148e59febb13f596167f0b95e11e0d3c7
Dery Tresna Pratama <tresnadery@gmail.com>
2023-08-05 07:19:49 +0700

    feat(dice game): implement pkg dice game

--- test_praktek/main.go
+++ test_praktek/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"qoin_test/test_praktek/util/dice_game"
 )
 
+var totalPlayer, totalDice int
+
 func main() {
-	fmt.Println("Hello World!")
+	err := getTotalPlayerAndTotalDice()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	game := dice_game.NewGame(totalPlayer, totalDice)
+	game.Start()
+}
+
+func getTotalPlayerAndTotalDice() error {
+	fmt.Print("Total pemain: ")
+	fmt.Scan(&totalPlayer)
+	fmt.Print("Total dadu: ")
+	fmt.Scan(&totalDice)
+	if totalPlayer < 1 {
+		return fmt.Errorf("total player kurang dari 1 orang")
+	}
+
+	if totalDice < 1 {
+		return fmt.Errorf("total dadu kurang dari 1 dadu")
+	}
+	return nil
 }
